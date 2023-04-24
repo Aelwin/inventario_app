16d931232890e6f977742d84768e7debb6e8852b
Aelwin <mithrandir01@hotmail.com>
2023-04-24 13:51:00 +0200

    comprobacion fecha no nula

--- app/src/main/java/com/aelwin/inventario/util/Utilidades.kt
+++ app/src/main/java/com/aelwin/inventario/util/Utilidades.kt
@@ -6,10 +6,12 @@ import java.time.format.DateTimeFormatter
 class Utilidades {
 
     companion object {
-        fun formatDateFromApi(fecha: String): String {
+        fun formatDateFromApi(fecha: String?): String {
             // Primero pasamos el string con formato del API a LocalDate y luego lo formateamos como queremos que se muestre
-            return LocalDate.parse(fecha, DateTimeFormatter.ofPattern(Constantes.FORMATO_FECHA_API))
+            return if (fecha != null)
+                LocalDate.parse(fecha, DateTimeFormatter.ofPattern(Constantes.FORMATO_FECHA_API))
                 .format(DateTimeFormatter.ofPattern(Constantes.FORMATO_FECHA))
+            else ""
         }
     }
 }
