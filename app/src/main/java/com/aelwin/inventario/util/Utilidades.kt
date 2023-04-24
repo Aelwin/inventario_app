39ea68de6c88c19e52cd51357af7657660276d8f
Aelwin <mithrandir01@hotmail.com>
2023-04-24 13:30:54 +0200

    recuperar fechas del api en su formato y convertirlas a otro

---
+++ app/src/main/java/com/aelwin/inventario/util/Utilidades.kt
@@ -0,0 +1,15 @@
+package com.aelwin.inventario.util
+
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+
+class Utilidades {
+
+    companion object {
+        fun formatDateFromApi(fecha: String): String {
+            // Primero pasamos el string con formato del API a LocalDate y luego lo formateamos como queremos que se muestre
+            return LocalDate.parse(fecha, DateTimeFormatter.ofPattern(Constantes.FORMATO_FECHA_API))
+                .format(DateTimeFormatter.ofPattern(Constantes.FORMATO_FECHA))
+        }
+    }
+}
