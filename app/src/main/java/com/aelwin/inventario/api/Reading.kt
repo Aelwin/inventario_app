d2bcc476554d28f9fdbac9e6077ce29a136ee1f2
Aelwin <mithrandir01@hotmail.com>
2023-04-24 13:33:36 +0200

    añadidas todas propiedades que devuelve el API

--- app/src/main/java/com/aelwin/inventario/api/Reading.kt
+++ app/src/main/java/com/aelwin/inventario/api/Reading.kt
@@ -1,9 +1,24 @@
 package com.aelwin.inventario.api
 
+import com.aelwin.inventario.util.Utilidades
 import com.google.gson.annotations.SerializedName
-import java.util.Date
 
-data class Reading(@SerializedName("valoracion") val valoracion: Int,
-                   @SerializedName("fecha_inicio") val fechaInicio: Date,
-                   @SerializedName("fecha_fin") val fechaFin: Date) {
+data class RatingReading(@SerializedName("valoracion") val valoracion: Int) {
+}
+
+data class Reading(@SerializedName("id") val id: Int,
+                    @SerializedName("valoracion") val valoracion: Int,
+                    @SerializedName("fecha_inicio") val fechaInicio: String,
+                    @SerializedName("fecha_fin") val fechaFin: String,
+                    @SerializedName("lector") val lector: String,
+                    @SerializedName("libro") val libro: Book) {
+
+    fun fechaInicioFormateada(): String {
+        return Utilidades.formatDateFromApi(fechaInicio)
+    }
+
+    fun fechaFinFormateada(): String {
+        return Utilidades.formatDateFromApi(fechaFin)
+    }
+
 }
