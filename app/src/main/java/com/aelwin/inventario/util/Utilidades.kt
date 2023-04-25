8ec60a612050f90903ccfc056c917ccf480769be
Aelwin <mithrandir01@hotmail.com>
2023-04-25 13:43:13 +0200

    nuevos metodos para tratar con fechas

--- app/src/main/java/com/aelwin/inventario/util/Utilidades.kt
+++ app/src/main/java/com/aelwin/inventario/util/Utilidades.kt
@@ -1,5 +1,6 @@
 package com.aelwin.inventario.util
 
+import android.widget.DatePicker
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 
@@ -12,6 +13,16 @@ class Utilidades {
                 LocalDate.parse(fecha, DateTimeFormatter.ofPattern(Constantes.FORMATO_FECHA_API))
                 .format(DateTimeFormatter.ofPattern(Constantes.FORMATO_FECHA))
             else ""
+        }
+
+        fun getLocalDateFromDatePicker(datePicker: DatePicker): LocalDate? {
+            return LocalDate.of(datePicker.year, datePicker.month, datePicker.dayOfMonth)
+        }
+
+        fun getStringDateFromDatePicker(datePicker: DatePicker): String? {
+            return getLocalDateFromDatePicker(datePicker)?.format(
+                DateTimeFormatter.ofPattern(Constantes.FORMATO_FECHA_API)
+            )
         }
     }
 }
