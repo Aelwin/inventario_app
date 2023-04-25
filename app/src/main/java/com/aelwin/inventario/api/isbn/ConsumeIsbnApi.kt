126f2b0b6c02c705f248821c74bdc181a3f08fc5
Aelwin <mithrandir01@hotmail.com>
2023-04-25 13:49:00 +0200

    eliminado log

--- app/src/main/java/com/aelwin/inventario/api/isbn/ConsumeIsbnApi.kt
+++ app/src/main/java/com/aelwin/inventario/api/isbn/ConsumeIsbnApi.kt
@@ -1,6 +1,5 @@
 package com.aelwin.inventario.api.isbn
 
-import android.util.Log
 import com.aelwin.inventario.util.Constantes
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -12,7 +11,6 @@ class ConsumeIsbnApi {
         private val retrofit = getRetrofit()
 
         suspend fun getImageUrl(isbn: String): String {
-            Log.i("Annatar", "isbn:$isbn")
             val myResponse = retrofit.create(IsbnApiService::class.java).getImageUrl("isbn:$isbn")
             return when {
                 myResponse.body()?.totalItems == 1 && myResponse.body()?.items?.get(0)?.volumeInfo?.imageLinks != null -> {
