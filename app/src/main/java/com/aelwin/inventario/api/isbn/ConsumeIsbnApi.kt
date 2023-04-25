c7765fd98fa49a96499a491ecd905ad54e8f80ea
Aelwin <mithrandir01@hotmail.com>
2023-04-25 13:42:13 +0200

    refactorizacion metodo recuperar imagen

--- app/src/main/java/com/aelwin/inventario/api/isbn/ConsumeIsbnApi.kt
+++ app/src/main/java/com/aelwin/inventario/api/isbn/ConsumeIsbnApi.kt
@@ -1,5 +1,6 @@
 package com.aelwin.inventario.api.isbn
 
+import android.util.Log
 import com.aelwin.inventario.util.Constantes
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -11,11 +12,15 @@ class ConsumeIsbnApi {
         private val retrofit = getRetrofit()
 
         suspend fun getImageUrl(isbn: String): String {
+            Log.i("Annatar", "isbn:$isbn")
             val myResponse = retrofit.create(IsbnApiService::class.java).getImageUrl("isbn:$isbn")
-            return if (myResponse.body()?.totalItems == 1) {
-                myResponse.body()!!.items[0].volumeInfo.imageLinks.urlImage.replace("http", "https")
-            } else {
-                "culquier cosa que no sea una url valida"
+            return when {
+                myResponse.body()?.totalItems == 1 && myResponse.body()?.items?.get(0)?.volumeInfo?.imageLinks != null -> {
+                    myResponse.body()!!.items[0].volumeInfo.imageLinks.urlImage.replace("http", "https")
+                }
+                else -> {
+                    "cualquier cosa que no sea una url valida"
+                }
             }
         }
 
