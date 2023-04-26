2de70d78251df92abf4f78880a151539c7ce6246
Aelwin <mithrandir01@hotmail.com>
2023-04-26 11:35:01 +0200

    nueva constante api local

--- app/src/main/java/com/aelwin/inventario/util/Constantes.kt
+++ app/src/main/java/com/aelwin/inventario/util/Constantes.kt
@@ -7,6 +7,7 @@ class Constantes {
         const val AUTHOR_ID: String = "AUTHOR_ID"
         const val DETA_APP: String = "https://24an09.deta.dev/"
         const val SPACE_APP: String = "https://inventario-1-u1736663.deta.app/"
+        const val LOCAL_APP: String = "http://10.0.2.2:8000/"
         const val ISBN_API_URL: String = "https://www.googleapis.com/books/v1/"
         const val FORMATO_FECHA: String = "dd/MM/yyyy"
         const val FORMATO_FECHA_API: String = "yyyy-MM-dd"
