d2c3328ac16a78349c11162c0897b524661422f2
Aelwin <juanlopez633@gmail.com>
2023-04-26 19:32:48 +0200

    comentada propiedad

--- app/src/main/java/com/aelwin/inventario/util/Constantes.kt
+++ app/src/main/java/com/aelwin/inventario/util/Constantes.kt
@@ -5,7 +5,7 @@ class Constantes {
     companion object {
         const val BOOK_ID: String = "BOOK_ID"
         const val AUTHOR_ID: String = "AUTHOR_ID"
-        const val DETA_APP: String = "https://24an09.deta.dev/"
+        //const val DETA_APP: String = "https://24an09.deta.dev/"
         const val SPACE_APP: String = "https://inventario-1-u1736663.deta.app/"
         const val LOCAL_APP: String = "http://10.0.2.2:8000/"
         const val ISBN_API_URL: String = "https://www.googleapis.com/books/v1/"
