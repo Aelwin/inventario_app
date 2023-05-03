f0cf818fcec5f5860d9be9ec4872e79425a49c87
Aelwin <mithrandir01@hotmail.com>
2023-05-03 08:39:24 +0200

    tener en cuenta propiedades que pueden ser nulas

--- app/src/main/java/com/aelwin/inventario/api/Book.kt
+++ app/src/main/java/com/aelwin/inventario/api/Book.kt
@@ -34,50 +34,50 @@ data class BookDetail(@SerializedName("id") val id: Int,
     }
 }
 
-data class BookCreate(@SerializedName("titulo") val titulo: String,
-                      @SerializedName("propietario") val propietario: String,
-                      @SerializedName("formato") val formato: String,
-                      @SerializedName("idioma") val idioma: String,
-                      @SerializedName("precio") val precio: Float,
-                      @SerializedName("isbn") val isbn: String,
-                      @SerializedName("sinopsis") val sinopsis: String,
-                      @SerializedName("imagen") val imagen: String,
-                      @SerializedName("editorial") val editorial: String,
-                      @SerializedName("fecha_compra") val fechaCompra: String,
-                      @SerializedName("observaciones") val observaciones: String,
-                      @SerializedName("categoria") val categoria: String,
-                      @SerializedName("saga") val saga: String,
+data class BookCreate(@SerializedName("titulo") val titulo: String?,
+                      @SerializedName("propietario") val propietario: String?,
+                      @SerializedName("formato") val formato: String?,
+                      @SerializedName("idioma") val idioma: String?,
+                      @SerializedName("precio") val precio: Float?,
+                      @SerializedName("isbn") val isbn: String?,
+                      @SerializedName("sinopsis") val sinopsis: String?,
+                      @SerializedName("imagen") val imagen: String?,
+                      @SerializedName("editorial") val editorial: String?,
+                      @SerializedName("fecha_compra") val fechaCompra: String?,
+                      @SerializedName("observaciones") val observaciones: String?,
+                      @SerializedName("categoria") val categoria: String?,
+                      @SerializedName("saga") val saga: String?,
                       @SerializedName("autores") val autores: List<AuthorBook>) {
 
     data class Builder(
-        var titulo: String = "",
-        var propietario: String = "",
-        var formato: String = "",
-        var idioma: String = "",
-        var precio: Float = 0.0f,
-        var isbn: String = "",
-        var sinopsis: String = "",
-        var imagen: String = "",
-        var editorial: String = "",
-        var fechaCompra: String = "",
-        var observaciones: String = "",
-        var categoria: String = "",
-        var saga: String = "",
+        var titulo: String? = null,
+        var propietario: String? = null,
+        var formato: String? = null,
+        var idioma: String? = null,
+        var precio: Float? = null,
+        var isbn: String? = null,
+        var sinopsis: String? = null,
+        var imagen: String? = null,
+        var editorial: String? = null,
+        var fechaCompra: String? = null,
+        var observaciones: String? = null,
+        var categoria: String? = null,
+        var saga: String? = null,
         var autores: List<AuthorBook> = listOf()
     ) {
-        fun withTitulo(titulo: String) = apply { this.titulo = titulo }
-        fun withPropietario(propietario: String) = apply { this.propietario = propietario }
+        fun withTitulo(titulo: String?) = apply { this.titulo = titulo }
+        fun withPropietario(propietario: String?) = apply { this.propietario = propietario }
         fun withFormato(formato: String) = apply { this.formato = formato }
         fun withIdioma(idioma: String) = apply { this.idioma = idioma }
-        fun withPrecio(precio: Float) = apply { this.precio = precio }
-        fun withIsbn(isbn: String) = apply { this.isbn = isbn }
-        fun withSinopsis(sinopsis: String) = apply { this.sinopsis = sinopsis }
-        fun withImagen(imagen: String) = apply { this.imagen = imagen }
-        fun withEditorial(editorial: String) = apply { this.editorial = editorial }
-        fun withFechaCompra(fechaCompra: String) = apply { this.fechaCompra = fechaCompra }
-        fun withObservaciones(observaciones: String) = apply { this.observaciones = observaciones }
-        fun withCategoria(categoria: String) = apply { this.categoria = categoria }
-        fun withSaga(saga: String) = apply { this.saga = saga }
+        fun withPrecio(precio: Float?) = apply { this.precio = precio }
+        fun withIsbn(isbn: String?) = apply { this.isbn = isbn }
+        fun withSinopsis(sinopsis: String?) = apply { this.sinopsis = sinopsis }
+        //fun withImagen(imagen: String?) = apply { this.imagen = imagen }
+        fun withEditorial(editorial: String?) = apply { this.editorial = editorial }
+        fun withFechaCompra(fechaCompra: String?) = apply { this.fechaCompra = fechaCompra }
+        fun withObservaciones(observaciones: String?) = apply { this.observaciones = observaciones }
+        fun withCategoria(categoria: String?) = apply { this.categoria = categoria }
+        fun withSaga(saga: String?) = apply { this.saga = saga }
         fun withAutores(autores: List<AuthorBook>) = apply { this.autores = autores }
 
         fun build() = BookCreate(
